c7a7ea5b000a1621c130df911bf27ea8e455e512
Mikkel Gravgaard <mikkel@klokke.dk>
2009-08-25 13:40:09 +0200

    version number test

--- contextlinky.py
+++ contextlinky.py
@@ -2,7 +2,7 @@ from waveapi import events
 from waveapi import model
 from waveapi import robot
 
-APP_VERSION = '3.1'
+APP_VERSION = '4'
 
 def OnParticipantsChanged(properties, context):
   """Invoked when any participants have been added/removed."""
