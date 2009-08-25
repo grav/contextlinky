64a910254f4e866eb528262d0161c61d9d57d693
Mikkel Gravgaard <mikkel@klokke.dk>
2009-08-25 12:35:40 +0200

    Responding to blip creation

--- contextlinky.py
+++ contextlinky.py
@@ -2,6 +2,8 @@ from waveapi import events
 from waveapi import model
 from waveapi import robot
 
+APP_VERSION = '2'
+
 def OnParticipantsChanged(properties, context):
   """Invoked when any participants have been added/removed."""
   added = properties['participantsAdded']
@@ -11,7 +13,14 @@ def OnParticipantsChanged(properties, context):
 def OnRobotAdded(properties, context):
   """Invoked when the robot has been added."""
   root_wavelet = context.GetRootWavelet()
-  root_wavelet.CreateBlip().GetDocument().SetText("I'm alive!")
+  root_wavelet.CreateBlip().GetDocument().SetText("I'm alive! Version" + APP_VERSION)
+
+def OnBlipCreated(properties, context):
+  root_wavelet = context.GetRootWavelet()
+  for blip in context.GetBlips():
+    text = blip.GetDocument().GetText()
+    if !text.find("Du sagde: ")
+      root_wavelet.CreateBlip().GetDocument().SetText("Du sagde: "+text)
 
 def Notify(context):
   root_wavelet = context.GetRootWavelet()
@@ -20,9 +29,10 @@ def Notify(context):
 if __name__ == '__main__':
   myRobot = robot.Robot('contextlinky', 
       image_url='http://contextlinky.appspot.com/icon.png',
-      version='1',
+      version=APP_VERSION,
       profile_url='http://contextlinky.appspot.com/')
   myRobot.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged)
   myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
+  myRobot.RegisterHandler(events.WAVELET_BLIP_CREATED, OnBlipCreated)
   myRobot.Run()
   
